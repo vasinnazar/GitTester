e74c01b3748cf106734a2be9997f98d0731be17f
Назар <vasinnazar@92gmail.com>
2020-10-25 15:27:22 +0700

    test for New.cs

---
+++ Assets/New.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("For our test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
